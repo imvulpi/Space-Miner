3d7cb670db27d70f3993000cc082f1b50df80d10
imvulpi <pjadasz07@gmail.com>
2024-08-20 22:47:01 +0200

    Added godot helpers
    Added UIPositionLocator and ItemList helpers
    UIPositionLocator locates a Control node in a specified Vector2 Position. It works based on Z-Index, highest at that position will get returned. Works on all UI control nodes.
    
    ItemList Helper can help you distinguish a scrollable itemlist from non-scrollable itemlist

---
+++ src/code/components/commons/godot/position_locator/UIPositionLocatorType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceMiner.src.code.components.commons.godot.position_locator
+{
+    public enum UIPositionLocatorType
+    {
+        Shallow,
+        DeepestNormal,
+        ZIndex,
+    }
+}
