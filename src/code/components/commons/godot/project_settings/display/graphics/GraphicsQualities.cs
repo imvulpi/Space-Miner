fc5dd579b67e93e37605fd93ef6b35a2889749c5
imvulpi <pjadasz07@gmail.com>
2024-08-21 19:42:32 +0200

    Update GraphicsQualities WIP

--- src/code/components/commons/godot/project_settings/display/graphics/GraphicsQualities.cs
+++ src/code/components/commons/godot/project_settings/display/graphics/GraphicsQualities.cs
@@ -7,10 +7,45 @@ using System.Threading.Tasks;
 
 namespace SpaceMiner.src.code.components.commons.godot.project_settings.display.graphics
 {
-    public class GraphicsQualities
+    public class GraphicsQualitiesHelper
     {
-        public const string LOW = "low";
-        public const string MEDIUM = "medium";
-        public const string HIGH = "high";
+        private const string LOW = "low";
+        private const string MEDIUM = "medium";
+        private const string HIGH = "high";
+        
+        public string GetStringGraphicsQuality(GraphicsQuality quality)
+        {
+            switch(quality)
+            {
+                case GraphicsQuality.High:
+                    return HIGH;
+                case GraphicsQuality.Medium:
+                    return MEDIUM;
+                case GraphicsQuality.Low:
+                    return LOW;
+            }
+            return null;
+        }
+
+        public GraphicsQuality GetGraphicsQuality(string quality)
+        {
+            switch (quality)
+            {
+                case HIGH:
+                    return GraphicsQuality.High;
+                case MEDIUM:
+                    return GraphicsQuality.Medium;
+                case LOW:
+                    return GraphicsQuality.Low;
+            }
+            return GraphicsQuality.High;
+        }
+    }
+
+    public enum GraphicsQuality
+    {
+        Low,
+        Medium,
+        High
     }
 }
