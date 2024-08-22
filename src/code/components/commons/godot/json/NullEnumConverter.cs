d793ad7030193f0a9afc096914fb20160374e0da
imvulpi <pjadasz07@gmail.com>
2024-08-22 22:19:41 +0200

    Added Json attributes and converters
    FallbackJsonConverter attribute - Provides a way to give a fallback value if the For now can only be used with converters which have a single variable in their constructor, the variable being the fallback value.
    
    DefaultEnumConverter - Used in the fallback attribute on Enums it has to be constructed with the fallback value. Places the fallback value when the conversion for string to Enum value fails.
    
    NullEnumConverter - Used on Enums, places a null if conversion of enum from string fails Can be used in simple JsonConverter attribute and requires that the enum property to be nullable by for example placing ? at the end.

---
+++ src/code/components/commons/godot/json/NullEnumConverter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace JsonEnumTest
+{
+    public class NullableEnumConverter<T> : JsonConverter<T?> where T : struct, Enum
+    {
+        public override T? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            if (reader.TokenType == JsonTokenType.Null)
+            {
+                return null;
+            }
+
+            if (reader.TokenType == JsonTokenType.String)
+            {
+                string enumString = reader.GetString();
+                if (Enum.TryParse<T>(enumString, ignoreCase: true, out var enumValue) && Enum.IsDefined(typeof(T), enumValue))
+                {
+                    return enumValue;
+                }
+            }
+
+            return null;
+        }
+
+        public override void Write(Utf8JsonWriter writer, T? value, JsonSerializerOptions options)
+        {
+            if (value.HasValue)
+            {
+                writer.WriteStringValue(value.Value.ToString());
+            }
+            else
+            {
+                writer.WriteNullValue();
+            }
+        }
+    }
+
+}
