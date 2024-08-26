faf3a3485ba5e87241c60e4cf2d9c05665e20017
imvulpi <pjadasz07@gmail.com>
2024-08-26 14:52:56 +0200

    Added log classes, Entry script and node
    Classes for logging errors, warnings and infos.
    
    EntryLoad script is the entry script which checks and loads necessary data,
    load.tscn - loading scene.
    For now there is no other scenes loaded after the entryload.
    
    Changed default enum converter to log different types of information
    Moved and changed UserSettingChecker to log different types of information

---
+++ src/code/components/processing/special/load/checkers/UserSettingChecker.cs
@@ -0,0 +1,91 @@
+﻿using Godot;
+using SpaceMiner.src.code.components.commons.errors;
+using SpaceMiner.src.code.components.commons.other.paths;
+using SpaceMiner.src.code.components.processing.data.settings.couplers;
+using System;
+using System.IO;
+using System.Threading;
+
+namespace SpaceMiner.src.code.components.processing.special.load.checkers
+{
+    public class UserSettingChecker
+    {
+        public UserSettingChecker()
+        {
+            
+        }
+
+        private readonly string UserSettingPath = Path.Join(Godot.OS.GetUserDataDir(), ExternalPaths.USER_SETTING);
+        private const int MAX_RETRY = 3;
+        public void Check()
+        {
+            GD.Print(new PrettyInfo(PrettyInfoType.Checking, $"{ExternalPaths.USER_SETTING}"));
+            UserSettings settings = new();
+            SettingCoupler coupler = new();
+            CheckFileExistance();
+            for(int i = 0; i < MAX_RETRY; i++)
+            {
+                (bool loaded, Exception ex) = CheckSettingLoad(settings, coupler);
+                if (loaded)
+                {
+                    GD.Print(new PrettyInfo(PrettyInfoType.Loaded, $"{ExternalPaths.USER_SETTING}"));
+                    break;
+                }
+                else
+                {
+                    GD.Print(new PrettyInfo(PrettyInfoType.Retry, $"{ExternalPaths.USER_SETTING}", $"User Settings repair and load retry ({i+1}/{MAX_RETRY})"));
+                    Thread.Sleep(100);
+                    TryRepairSettingLoad(settings, coupler);
+                }
+
+                if(i+1 >= MAX_RETRY)
+                {
+                    (loaded, ex) = CheckSettingLoad(settings, coupler);
+                    if (loaded)
+                    {
+                        GD.Print(new PrettyInfo(PrettyInfoType.Loaded, $"{ExternalPaths.USER_SETTING}"));
+                    }
+                    else
+                    {
+                        GD.PushError(new PrettyError(PrettyErrorType.Failed, $"{ex.Message}", $"Repairing {ExternalPaths.USER_SETTING} failed, loading was unsuccesful"));
+                    }
+                }
+            }
+        }
+
+        private void CheckFileExistance()
+        {
+            if (UserSettingPath != null && !File.Exists(UserSettingPath))
+            {
+                GD.PushWarning(new PrettyWarning(PrettyWarningType.NotFound, $"{ExternalPaths.USER_SETTING}"));
+                try
+                {
+                    File.Create(UserSettingPath).Close();
+                    GD.Print(new PrettyInfo(PrettyInfoType.Created, $"{ExternalPaths.USER_SETTING}", "Succesfully created"));
+                }
+                catch (Exception ex)
+                {
+                    GD.PushError(new PrettyError(PrettyErrorType.Failed, $"{ExternalPaths.USER_SETTING}", $"Creation failed, message: {ex.Message}"));
+                }
+            }
+        }
+
+        private (bool loaded, Exception exception) CheckSettingLoad(UserSettings settings, SettingCoupler coupler)
+        {
+            try
+            {
+                coupler.Load(settings, UserSettingPath);
+                return (true, null);
+            }
+            catch (Exception ex)
+            {
+                return (false, ex);
+            }
+        }
+
+        private void TryRepairSettingLoad(UserSettings settings, SettingCoupler coupler)
+        {
+            coupler.Save(settings, UserSettingPath);
+        }
+    }
+}
