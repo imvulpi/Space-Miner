b389611fd5157d297930a0aa661dd938b90e8dfb
imvulpi <pjadasz07@gmail.com>
2024-08-28 08:27:06 +0200

    Added entry checks.
    Added EssentialDIrectoryChecker - now checks UserDataDir, and SavesDir
    Removed temp_game.tscn
    Added premade.tscn which is a hand made campaign map.

--- src/code/components/processing/special/load/EntryLoad.cs
+++ src/code/components/processing/special/load/EntryLoad.cs
@@ -4,12 +4,24 @@ using SpaceMiner.src.code.components.processing.special.load.checkers;
 
 public partial class EntryLoad : Node
 {
+    [Export] public PackedScene MainMenuScene { get; set; }
     public override void _Ready()
 	{
         GD.Print(new PrettyInfo(PrettyInfoType.Broadcast, "Entry", "Entry process started."));
+
+        // Checking
+        new EssentialDirectoryChecker().Check();
         new UserSettingChecker().Check();
 
-		GD.Print(new PrettyInfo(PrettyInfoType.Broadcast, "Entry", "Entry process ended."));
+        // Loading
+        CallDeferred("LoadMainMenu");
+        GD.Print(new PrettyInfo(PrettyInfoType.Broadcast, "Entry", "Entry process ended."));
+        QueueFree();
+    }
+
+    private void LoadMainMenu()
+    {
+        GetTree().Root.AddChild(MainMenuScene.Instantiate());
     }
 
     public override void _Process(double delta)
---
+++ src/code/components/processing/special/load/checkers/EssentialDirectoryChecker.cs
@@ -0,0 +1,52 @@
+﻿using Godot;
+using SpaceMiner.src.code.components.commons.errors;
+using SpaceMiner.src.code.components.commons.other.IO;
+using SpaceMiner.src.code.components.commons.other.paths.external_paths;
+using System;
+using System.IO;
+
+namespace SpaceMiner.src.code.components.processing.special.load.checkers
+{
+    public class EssentialDirectoryChecker
+    {
+        public EssentialDirectoryChecker() { }
+
+        public void Check()
+        {
+            string userPath = OS.GetUserDataDir();
+            CheckDirectory(userPath);
+
+            string savesDirectory = Path.Combine(userPath, ExternalPaths.SAVES_DIR);
+            CheckDirectory(savesDirectory);
+        }
+
+        public bool CheckDirectory(string directoryPath)
+        {
+            GD.Print(new PrettyInfo(PrettyInfoType.Checking, $"{directoryPath}"));
+            string[] dirs = DirectoryHelper.GetParentDirectories(directoryPath);
+            string continuousPath = "";
+            for (int i = 0; i < dirs.Length; i++)
+            {
+                string currentPath = dirs[i];
+                continuousPath = Path.Join(continuousPath, currentPath);
+                if (!Directory.Exists(continuousPath))
+                {
+                    GD.PushWarning(new PrettyWarning(PrettyWarningType.NotFound, $"{continuousPath}", "Essential directory does not exist."));
+                    try
+                    {
+                        GD.Print(new PrettyInfo(PrettyInfoType.RepairAttempt, $"{continuousPath}", $"Directory Repair Attempt"));
+                        Directory.CreateDirectory(continuousPath);
+                        GD.Print(new PrettyInfo(PrettyInfoType.Successful, $"{continuousPath}", $"Directory repaired!"));
+                    }
+                    catch (Exception ex)
+                    {
+                        GD.PushError(new PrettyError(PrettyErrorType.Error, $"{ex.Message}"));
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+    }
+}
