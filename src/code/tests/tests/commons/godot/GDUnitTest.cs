83638d87c39e2836686bdceb78a489ddf7a017b5
imvulpi <78976654+imvulpi@users.noreply.github.com>
2025-02-23 11:11:00 +0100

    Added GdUnit
    Added GdUnit and some GDunit test to test the GdUnit working.
    A lot of GDunit in this text.

---
+++ src/code/tests/tests/commons/godot/GDUnitTest.cs
@@ -0,0 +1,30 @@
+﻿using GdUnit4;
+using Godot;
+using SpaceMiner.src.code.components.commons.godot.position_locator;
+using System.Collections.Generic;
+
+namespace SpaceMinerTests.tests.commons.godot
+{
+    [TestSuite]
+    class GDUnitTest
+    {
+        [Before]
+        public void InitializeTest()
+        {
+
+        }
+
+        [TestCase]
+        public void GetChildNodeAtPositionTest()
+        {
+            GD.Print("TEST");
+            Assertions.AssertThat("E").Equals("E");
+        }
+
+        [After]
+        public void CleanupTest()
+        {
+
+        }
+    }
+}
---
+++ tests/commons/godot/GDUnitTest.cs
@@ -0,0 +1,29 @@
+﻿using GdUnit4;
+using Godot;
+using SpaceMiner.src.code.components.commons.godot.position_locator;
+using System.Collections.Generic;
+
+namespace SpaceMinerTests.tests.commons.godot
+{
+    [TestSuite]
+    class GDUnitTest
+    {
+        [Before]
+        public void InitializeTest()
+        {
+
+        }
+
+        [TestCase]
+        public void GetChildNodeAtPositionTest()
+        {
+            Assertions.AssertThat("This").Equals("This");
+        }
+
+        [After]
+        public void CleanupTest()
+        {
+
+        }
+    }
+}
